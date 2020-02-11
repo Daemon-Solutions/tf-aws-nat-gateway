806e18888450a0337c6ea56a12ff84ce13ec4f3a
Steven Whitaker <steven.whitaker@claranet.uk>
2020-02-11 12:12:26 +0000

    TF updating to be 0.12 compatible

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # nat gateway resources
 resource "aws_route" "private_nat_gateway" {
-  route_table_id            = "${element(var.private_route_table_ids, count.index)}"
+  route_table_id            = var.private_route_table_ids[count.index]
   destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id            = "${element(aws_nat_gateway.natgw.*.id, count.index)}"
-  count                     = "${length(var.private_subnets)}"
+  nat_gateway_id            = aws_nat_gateway.natgw.*.id[count.index]
+  count                     = length(var.private_subnets)
 }
 
 resource "aws_eip" "nateip" {
   vpc                       = true
-  count                     = "${length(var.private_subnets)}"
+  count                     = length(var.private_subnets)
 }
 
 resource "aws_nat_gateway" "natgw" {
-  allocation_id             = "${element(aws_eip.nateip.*.id, count.index)}"
-  subnet_id                 = "${element(var.public_subnet_ids, count.index)}"
-  count                     = "${length(var.private_subnets)}"
+  allocation_id             = aws_eip.nateip.*.id[count.index]
+  subnet_id                 = var.public_subnet_ids[count.index]
+  count                     = length(var.private_subnets)
 }
